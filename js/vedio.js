e615cfde06d3a566986e3d6501cda1e48469d777
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-02-04 11:51:57 +0600

    load fetch

--- js/vedio.js
+++ js/vedio.js
@@ -1,1 +1,18 @@
-console.log('yes this is vedio page')
+// Get load data form fetch
+const loadData= ()=>{
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then(res=>res.json())
+    .then(data=>loadCategories(data))
+    .catch(err=>console.log(err))
+}
+
+// loadCategories
+
+function loadCategories(data){
+
+    console.log(data.categories)
+
+}
+
+
+loadData();
